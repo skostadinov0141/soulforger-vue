c6b4b729f5715f85f9f65791210aec0fe0ce1878
Simeon Kostadinov <simeon.kostadinov0141@gmail.com>
2022-11-30 08:18:07 +0100

    added hero skeleton and contant, style to follow

---
+++ src/components/start_page/hero_section.vue
@@ -0,0 +1,35 @@
+
+
+<template>
+<div class="hero-alignment-container">
+    <h1>
+        Die Zukunft von Shadowrun beginnt mit Soulforger.
+    </h1>
+    <p>
+        Jede Geschichte beginnt mit einem Helden... diese Geschichte beginnt mit Soulforger. Soulforger ist eine Open-Source-Plattform, die derzeit von einem einzigen Entwickler und einem einzigen Designer betrieben wird. Alles, was wir erschaffen, ist aus Liebe zu Shadowrun und den Geschichten, die man damit erzählen kann, entstanden. Das Ziel ist es, es jedem zu ermöglichen, diese Geschichten zu erzählen, indem wir uns um den schwierigen Teil kümmern, der die Charaktererstellung, das Kampagnenmanagement und das ständige Suchen in Büchern beinhaltet.
+    </p>
+</div>
+</template>
+
+
+
+<style scoped>
+
+    .hero-alignment-container{
+        display: flex;
+        flex-direction: column;
+        width: 400px;
+        margin-left: auto;
+    }
+
+    @media only screen and (max-width: 1024px){
+        .hero-alignment-container{
+        display: flex;
+        flex-direction: column;
+        width: 500px;
+        margin-left: auto;
+        margin-right: auto;
+    }
+    }
+
+</style>
--- src/views/StartScreen.vue
+++ src/views/StartScreen.vue
@@ -1,15 +1,49 @@
 <script lang="ts">
 
+import { defineComponent } from "vue";
+import HeroSection from "../components/start_page/hero_section.vue"
+
+export default defineComponent({
+    data(){
+
+    },
+    components:{
+        HeroSection
+    }
+})
+
 </script>
 
 
 <template>
-<h1>
-    TEST
-</h1>
+<div class="focus-padding">
+    <section class="hero-container">
+        <HeroSection/>
+    </section>
+    <section class="goals-container">
+        <HeroSection/>
+    </section>
+    <section class="account-application-container">
+        <HeroSection/>
+    </section>
+</div>
 </template>
 
 
 <style scoped>
+    .focus-padding{
+        display: flex;
+        flex-direction: column;
+        padding-left: 16vw;
+        padding-right: 16vw;
+    }
+
+    .hero-container{
+        display: flex;
+        align-items: center;
+        max-width: 100vw;
+        min-height: 100vh;
+        z-index: 0;
+    }
 
 </style>
